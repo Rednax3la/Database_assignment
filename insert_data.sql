608bd5071a9f2f125d5f91ac01053a4ed8ab6f0a
Rednax3la <132368427+Rednax3la@users.noreply.github.com>
2025-04-13 16:41:36 -0400

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,8 @@
+-- Insert sample data into the tables
+
+INSERT INTO book_language (language_name) VALUES ('English'), ('Spanish'), ('French');
+INSERT INTO publisher (name) VALUES ('Penguin'), ('HarperCollins'), ('Random House');
+INSERT INTO country (country_name) VALUES ('USA'), ('Canada'), ('UK');
+INSERT INTO address_status (status_name) VALUES ('Current'), ('Old');
+
+-- Add more sample data as needed
